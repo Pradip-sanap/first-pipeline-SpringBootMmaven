d3f34f8f1f0c01f37d1a18f64ea82d4d92e5d550
Pradip-sanap <102137666+Pradip-sanap@users.noreply.github.com>
2023-09-09 19:06:38 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello World! welcome. thank you.";
+        return "Hello World!";
     }
 
 }
